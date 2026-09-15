9b54b8988309a4c1386ef42daf1d3893878a1747
Suamoxi <79272028+Suamoxi@users.noreply.github.com>
2026-09-15 10:47:51 +0200

    Export D2 split utilities

--- dgn4avbp/data/__init__.py
+++ dgn4avbp/data/__init__.py
@@ -5,6 +5,17 @@ from .avbp_hdf5 import (
     validate_snapshot_dataset,
     write_data_manifest,
 )
+from .splits import (
+    create_contiguous_split_manifest,
+    file_list_fingerprint,
+    load_split_config,
+    load_split_manifest,
+    order_files_by_iteration,
+    parse_snapshot_iteration,
+    split_indices_from_manifest,
+    validate_split_manifest,
+    write_split_manifest,
+)
 
 __all__ = [
     "AVBPHDF5FixedMeshDataset",
@@ -12,4 +23,13 @@ __all__ = [
     "load_data_config",
     "validate_snapshot_dataset",
     "write_data_manifest",
+    "create_contiguous_split_manifest",
+    "file_list_fingerprint",
+    "load_split_config",
+    "load_split_manifest",
+    "order_files_by_iteration",
+    "parse_snapshot_iteration",
+    "split_indices_from_manifest",
+    "validate_split_manifest",
+    "write_split_manifest",
 ]
